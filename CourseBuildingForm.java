fc1b043f05b0ab1104c5d78ece7559cb421b28c7
Richard Hsu <rhsu0268@gmail.com>
2017-02-08 12:51:02 -0500

    Added code for Math

--- CourseBuildingForm.java
+++ CourseBuildingForm.java
@@ -14,6 +14,15 @@ public class CourseBuildingForm extends CourseBuildingHandle
             newCourse = new CSCourse(csCourseFactory);
             newCourse.setName("Object-Oriented-Design");
         }
+        else
+        {
+            System.out.println("Course Type: Math");
+            MathCourseFactory csCourseFactory = new MathCourseFactory();
+
+            // polymorphism
+            newCourse = new CSCourse(csCourseFactory);
+            newCourse.setName("Algebra");
+        }
         return newCourse;
     }
 }
---
+++ MathCourseFactory.java
@@ -0,0 +1,11 @@
+public class MathCourseFactory implements CourseFactory
+{
+    public void addCodingLab()
+    {
+        System.out.println("Coding Lab : NO");
+    }
+    public void addCodingProject()
+    {
+        System.out.println("Coding Project : NO");
+    }
+}
